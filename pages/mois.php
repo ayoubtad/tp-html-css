06358a162c24ac7344d746a3749c305e5ef81b11
Ayoub Tadili <ayoubtadili139@gmail.com>
2026-03-12 18:34:43 +0000

    modifications du site: eau, mois, table de multiplication

--- pages/mois.php
+++ pages/mois.php
@@ -8,7 +8,56 @@
     <link rel="stylesheet" href="../css/style_mois.css">
 </head>
 
-<body>
+<?php
+$mois = 1;
+$annee = 2000;
+$nom_mois = "Janvier";
+
+// Déterminer la saison basée sur le mois affiché
+switch ($mois) {
+    case 12:
+    case 1:
+    case 2:
+        $saison = 'hiver';
+        break;
+    case 3:
+    case 4:
+    case 5:
+        $saison = 'printemps';
+        break;
+    case 6:
+    case 7:
+    case 8:
+        $saison = 'ete';
+        break;
+    case 9:
+    case 10:
+    case 11:
+        $saison = 'automne';
+        break;
+}
+
+// Calculer le nombre de jours
+switch ($mois) {
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        $jours = 30;
+        break;
+    case 2:
+        if ($annee % 4 == 0) {
+            $jours = 29;
+        } else {
+            $jours = 28;
+        }
+        break;
+    default:
+        $jours = 31;
+}
+?>
+
+<body class="saison-<?php echo $saison; ?>">
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
@@ -22,30 +71,6 @@
 
     <main>
         <h1>Nombre de jours par mois</h1>
-
-        <?php
-        $mois = 12;
-        $annee = 2000;
-        $nom_mois = "Décembre";
-
-        switch ($mois) {
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                $jours = 30;
-                break;
-            case 2:
-                if ($annee % 4 == 0) {
-                    $jours = 29;
-                } else {
-                    $jours = 28;
-                }
-                break;
-            default:
-                $jours = 31;
-        }
-        ?>
 
         <section>
             <p>Le mois de <strong><?php echo $nom_mois; ?></strong> de l'année <strong><?php echo $annee; ?></strong> a <strong><?php echo $jours; ?></strong> jours.</p>
