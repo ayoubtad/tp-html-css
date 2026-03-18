adbf266b1ba2a5b47e5f6ec4cf49745a6b3e1262
Ayoub Tadili <ayoubtadili139@gmail.com>
2026-03-18 11:16:03 +0000

    mod0{fication

--- pages/eau.php
+++ pages/eau.php
@@ -9,23 +9,23 @@
 
 <?php
 $temperature = 200;
-$classe = "";
+$eau = "";
 
 if ($temperature < 0) {
         $titre = "glacée";
-        $classe = "glacee";
+        $eau = "glacee";
 } else if ($temperature >= 0 && $temperature < 25) {
         $titre = "froide";
-        $classe = "froide";
+        $eau = "froide";
 } else if ($temperature >= 25 && $temperature < 50) {
         $titre = "chaude";
-        $classe = "chaude";
+        $eau = "chaude";
 } else if ($temperature >= 50 && $temperature < 100) {
         $titre = "brulante";
-        $classe = "brulante";
+        $eau = "brulante";
 } else {
         $titre = "vapeur";
-        $classe = "vapeur";
+        $eau = "vapeur";
 }
 
 // Déterminer la saison basée sur le mois actuel
@@ -33,26 +33,26 @@ $mois_actuel = date('n');
 $saison = '';
 
 switch ($mois_actuel) {
-    case 12:
-    case 1:
-    case 2:
-        $saison = 'hiver';
-        break;
-    case 3:
-    case 4:
-    case 5:
-        $saison = 'printemps';
-        break;
-    case 6:
-    case 7:
-    case 8:
-        $saison = 'ete';
-        break;
-    case 9:
-    case 10:
-    case 11:
-        $saison = 'automne';
-        break;
+        case 12:
+        case 1:
+        case 2:
+                $saison = 'hiver';
+                break;
+        case 3:
+        case 4:
+        case 5:
+                $saison = 'printemps';
+                break;
+        case 6:
+        case 7:
+        case 8:
+                $saison = 'ete';
+                break;
+        case 9:
+        case 10:
+        case 11:
+                $saison = 'automne';
+                break;
 }
 ?>
 
@@ -71,8 +71,8 @@ switch ($mois_actuel) {
         <main>
                 <h1><?php echo $titre; ?></h1>
                 <p class="temperature"><?php echo $temperature; ?>°C</p>
-                
-                
+
+
         </main>
 </body>
 
